18305172ef2bcf48e4788f5bcd2fefa3c0e983a7
John L. Armstrong IV <jlarmstrongiv@gmail.com>
2020-11-27 10:55:59 -0500

    fix: remove tarpath export

--- constants.js
+++ constants.js
@@ -4,4 +4,4 @@ const TFJS_PATH = '/tmp/tfjs-node'
 // const TAR_PATH = pathResolve(__dirname, 'tfjs-node.br')
 // const TAR_PATH = '/tmp/tfjs-node.br'
 
-module.exports = { TFJS_PATH, TAR_PATH }
+module.exports = { TFJS_PATH }
